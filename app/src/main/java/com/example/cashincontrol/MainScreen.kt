cd5ac9cfbb894625499a304adeba0f8d921bd6ff
Vladislav Yegorov <77292451+sckmat@users.noreply.github.com>
2024-10-27 15:38:27 +0500

    Main screen

--- app/src/main/java/com/example/cashincontrol/MainScreen.kt
+++ app/src/main/java/com/example/cashincontrol/MainScreen.kt
@@ -1,19 +1,95 @@
 package com.example.cashincontrol
 
-import android.annotation.SuppressLint
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Start() {
-    val navController = rememberNavController()
-    Scaffold(
-        bottomBar = {
-            BottomNavigation(navController = navController)
-        }
+fun MainScreen(){
+    TopSection()
+}
+
+@Preview
+@Composable
+fun TopSection() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
     ) {
-        NavGraph(navHostController = navController)
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.icon_top_mian),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(45.dp),
+                tint = Color.Unspecified
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text(
+                text = "3 дня Вы ведёте бюджет!",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Normal
+            )
+        }
+
+        Spacer(modifier = Modifier.height(43.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = "Баланс счёта",
+                fontSize = 20.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .background(Color(0xFFDCFFBB), shape = RoundedCornerShape(3.dp))
+                    .padding(horizontal = 8.dp, vertical = 4.dp)
+            )
+
+            Text(
+                text = "500 ₽",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Normal,
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // История операций
+
+        Text(
+            text = "История операций",
+            color = Color.Black,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .background(Color(0xFFDCFFBB), shape = RoundedCornerShape(3.dp))
+                .padding(horizontal = 8.dp, vertical = 4.dp)
+        )
+        Spacer(modifier = Modifier.height(6.dp))
     }
 }

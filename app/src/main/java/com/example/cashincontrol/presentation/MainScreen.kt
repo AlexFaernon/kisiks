6bda7ebbb11f69534335b6be6554a53348364059
Vladislav Yegorov <77292451+sckmat@users.noreply.github.com>
2024-10-28 17:00:51 +0500

    TransactionCard

--- app/src/main/java/com/example/cashincontrol/presentation/MainScreen.kt
+++ app/src/main/java/com/example/cashincontrol/presentation/MainScreen.kt
@@ -5,12 +5,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,12 +29,18 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cashincontrol.R
 
+@Preview
 @Composable
 fun MainScreen(){
-    TopSection()
+    Column(
+        verticalArrangement = Arrangement.spacedBy(23.dp) // Отступы между элементами
+    ) {
+        TopSection()
+        TransactionCard()
+    }
+
 }
 
-@Preview
 @Composable
 fun TopSection() {
     Column(
@@ -96,6 +107,83 @@ fun TopSection() {
 }
 
 @Composable
-fun ItemCard(){
+fun TransactionCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+        ) {
+            // Дата транзакции
+            Text(
+                text = "16 сентября",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                modifier = Modifier.padding(bottom = 5.dp)
+            )
 
+            Row(
+                verticalAlignment = Alignment.Top
+            ) {
+                // Иконка категории
+                Icon(
+                    painter = painterResource(id = R.drawable.icon_main),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(30.dp),
+                    tint = Color.Unspecified
+                )
+
+                Spacer(modifier = Modifier.width(9.dp))
+
+                // Текстовая информация о транзакции
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(
+                        text = "Здоровье и красота",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+
+                    Text(
+                        text = "Карта МИР",
+                        fontSize = 16.sp,
+                        color = Color.Gray
+                    )
+
+                    Text(
+                        text = "Аптека Улыбка",
+                        fontSize = 16.sp,
+                        color = Color.Gray
+                    )
+                }
+
+                // Сумма транзакции
+                Text(
+                    text = "-920₽",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier.padding(start = 6.dp)
+                )
+            }
+        }
+    }
 }
+
+//@Composable
+//fun TransactionListScreen(transactions: List<Transaction>) {
+//    LazyColumn(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .padding(16.dp),
+//        verticalArrangement = Arrangement.spacedBy(13.dp)
+//    ) {
+//        items(transactions) { transaction ->
+//            TransactionCard(transaction = transaction)
+//        }
+//    }
+//}

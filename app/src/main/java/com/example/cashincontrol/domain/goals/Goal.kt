1ce4d4b74855ce57a083257c150f57e2e47ae0d9
AlexFaernon <alexpavlenko6666@rambler.ru>
2024-12-16 17:38:52 +0500

    SCRUM-68 payments and inflation in goals

--- app/src/main/java/com/example/cashincontrol/domain/goals/Goal.kt
+++ app/src/main/java/com/example/cashincontrol/domain/goals/Goal.kt
@@ -14,9 +14,26 @@ data class Goal(
     @Serializable(with = LocalDateSerializer::class)
     val startDate: LocalDate = LocalDate.now()){
 
+    private val payments: MutableList<Pair<@Serializable(with = LocalDateSerializer::class) LocalDate, Float>> = mutableListOf()
     fun monthlyPayment(): Float{
         val months = Period.between(startDate, targetDate).toTotalMonths()
         return sum / months
     }
+
+    fun getInflationPayments(): List<Float>{
+        val months = Period.between(startDate, targetDate).toTotalMonths()
+        val result = mutableListOf(monthlyPayment())
+
+        for (i in 1..<months){
+            val prev = result.last()
+            result.add(prev * (Inflation.GlobalInflation + 1))
+        }
+
+        return result
+    }
+
+    fun addPayment(date: LocalDate, sum: Float) = payments.add(Pair(date, sum))
+
+    fun getPayments(): List<Pair<LocalDate, Float>> = payments
 }
 
